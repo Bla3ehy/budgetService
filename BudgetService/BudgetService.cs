26bb289f73d391618cb582474b602e836b65305a
User <aa4152@gmail.com>
2022-10-08 17:18:12 +0800

    get cross month and get cross two month

--- BudgetService/BudgetService.cs
+++ BudgetService/BudgetService.cs
@@ -5,6 +5,7 @@ namespace BudgetService;
 public class BudgetService
 {
     private IBudgetRepo _budgetRepo;
+
     public BudgetService(IBudgetRepo repo)
     {
         _budgetRepo = repo;
@@ -14,21 +15,53 @@ public class BudgetService
     {
         var budgets = _budgetRepo.GetAll();
         var result = decimal.Zero;
+        if (start.Month != end.Month)
+        {
+            var startDate = DateTime.ParseExact(start.ToString("yyyyMM") + "01", "yyyyMMdd", CultureInfo.CurrentCulture);
+            var endDate = DateTime.ParseExact(end.ToString("yyyyMM") + "01", "yyyyMMdd", CultureInfo.CurrentCulture);
+            var budgetList = budgets.Where(o =>
+                    startDate <= DateTime.ParseExact(o.YearMonth + "01", "yyyyMMdd", CultureInfo.CurrentCulture)
+                    && endDate >= DateTime.ParseExact(o.YearMonth + "01", "yyyyMMdd",
+                        CultureInfo.CurrentCulture))
+                .ToList();
+
+            foreach (var budget in budgetList)
+            {
+                if (budget.YearMonth == start.ToString("yyyyMM"))
+                {
+                    result += budget.Amount / DateTime.DaysInMonth(start.Year, start.Month) *
+                              (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1);
+                    continue;
+                }
+
+                if (budget.YearMonth == end.ToString("yyyyMM"))
+                {
+                    result += budget.Amount / DateTime.DaysInMonth(end.Year, end.Month) * end.Day;
+                    continue;
+                }
+
+                result += budget.Amount;
+            }
+
+            return result;
+        }
+
         if (start.Day >= 1 || end.Day <= DateTime.DaysInMonth(end.Year, end.Month))
         {
-            var monthBudget = budgets.Where(o => start == DateTime.ParseExact(o.YearMonth+"01","yyyyMMdd",CultureInfo.CurrentCulture))
+            var monthBudget = budgets.Where(o =>
+                    start == DateTime.ParseExact(o.YearMonth + "01", "yyyyMMdd", CultureInfo.CurrentCulture))
                 .Select(o => Convert.ToDecimal(o.Amount))
                 .First();
 
             result = monthBudget / DateTime.DaysInMonth(end.Year, end.Month) * (end.Day - start.Day + 1);
-
         }
+
         if (start.Day == 1 && end.Day == DateTime.DaysInMonth(end.Year, end.Month))
         {
-            
-           result = budgets.Where(o => start == DateTime.ParseExact(o.YearMonth+"01","yyyyMMdd",CultureInfo.CurrentCulture))
-               .Select(o => Convert.ToDecimal(o.Amount))
-               .First();
+            result = budgets.Where(o =>
+                    start == DateTime.ParseExact(o.YearMonth + "01", "yyyyMMdd", CultureInfo.CurrentCulture))
+                .Select(o => Convert.ToDecimal(o.Amount))
+                .First();
         }
 
         return result;
--- budgetServiceTest/UnitTest1.cs
+++ budgetServiceTest/UnitTest1.cs
@@ -30,10 +30,10 @@ public class Tests
                 Amount = 3100
             }
         });
-        var result = _budgetService.Query(start,end);
+        var result = _budgetService.Query(start, end);
         result.Should().Be(3100);
     }
-    
+
     [Test]
     public void Budget_get_partial_month()
     {
@@ -47,7 +47,56 @@ public class Tests
                 Amount = 3100
             }
         });
-        var result = _budgetService.Query(start,end);
+        var result = _budgetService.Query(start, end);
         result.Should().Be(500);
+    }
+
+    [Test]
+    public void Budget_get_cross_month()
+    {
+        var start = new DateTime(2022, 10, 31);
+        var end = new DateTime(2022, 11, 5);
+        _budgetRepo.GetAll().Returns(new List<Budget>
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 3100
+            },
+            new Budget()
+            {
+                YearMonth = "202211",
+                Amount = 300
+            },
+        });
+        var result = _budgetService.Query(start, end);
+        result.Should().Be(100 + 50);
+    }
+
+    [Test]
+    public void Budget_get_cross_two_month()
+    {
+        var start = new DateTime(2022, 10, 31);
+        var end = new DateTime(2022, 12, 5);
+        _budgetRepo.GetAll().Returns(new List<Budget>
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 3100
+            },
+            new Budget()
+            {
+                YearMonth = "202211",
+                Amount = 300
+            },
+            new Budget()
+            {
+                YearMonth = "202212",
+                Amount = 31
+            }
+        });
+        var result = _budgetService.Query(start, end);
+        result.Should().Be(100 + 300 + 5);
     }
 }
